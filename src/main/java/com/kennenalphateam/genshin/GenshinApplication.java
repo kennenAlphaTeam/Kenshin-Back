57306303573e0da9e64818125c07fda404c9bab6
skdlsco <nicnic4102@gmail.com>
2022-01-07 14:15:07 +0900

    init project

---
+++ src/main/java/com/kennenalphateam/genshin/GenshinApplication.java
@@ -0,0 +1,13 @@
+package com.kennenalphateam.genshin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kennenalphateam/genshin/GenshinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennenalphateam.genshin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
