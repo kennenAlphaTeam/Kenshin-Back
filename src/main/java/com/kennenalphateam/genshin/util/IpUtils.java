d15bd68cbf435d5f3c294afe6a5afd92f9b3584f
skdlsco <nicnic4102@gmail.com>
2022-02-17 12:49:45 +0900

    Http log 추가 - #10

---
+++ src/main/java/com/kennenalphateam/genshin/util/IpUtils.java
@@ -0,0 +1,28 @@
+package com.kennenalphateam.genshin.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpUtils {
+
+    public static String getIpFromRequest(HttpServletRequest request) {
+        String ip = request.getHeader("X-Forwarded-For");
+
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+
+        return ip;
+    }
+}
