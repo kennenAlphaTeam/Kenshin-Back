9d31d0c7bdf47fae1d562fc7525cf2e03a5826b3
skdlsco <nicnic4102@gmail.com>
2022-02-22 16:52:51 +0900

    springdoc swagger ui 추가 - #17

--- src/main/java/com/kennenalphateam/genshin/error/ErrorController.java
+++ src/main/java/com/kennenalphateam/genshin/error/ErrorController.java
@@ -1,5 +1,6 @@
 package com.kennenalphateam.genshin.error;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.boot.autoconfigure.web.servlet.error.AbstractErrorController;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Hidden
 public class ErrorController  extends AbstractErrorController {
     public ErrorController(ErrorAttributes errorAttributes) {
         super(errorAttributes);
