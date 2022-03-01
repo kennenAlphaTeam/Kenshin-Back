e46f3794b8f50aa080d94e34239f8b82ea6e79a1
InChae Na <nicnic4102@gmail.com>
2022-03-01 16:43:28 +0900

    port가 8080이 아닐 때도 oauth test을 성공하도록 수정 - #27 (#29)

--- src/test/java/com/kennenalphateam/genshin/auth/GoogleOauthTests.java
+++ src/test/java/com/kennenalphateam/genshin/auth/GoogleOauthTests.java
@@ -1,8 +1,10 @@
 package com.kennenalphateam.genshin.auth;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -10,14 +12,22 @@ import static org.hamcrest.CoreMatchers.containsString;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class GoogleOauthTests {
 
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    public void beforeAll() {
+        RestAssured.port = port;
+    }
+
     @Test
     public void google로그인_시도하면_Oauth인증창_반환한다() {
         given()
                 .when()
-                    .redirects().follow(false)
-                    .get("auth/oauth/login/google")
+                .redirects().follow(false)
+                .get("auth/oauth/login/google")
                 .then()
-                    .statusCode(302)
-                    .header("Location", containsString("https://accounts.google.com/o/oauth2/v2/auth"));
+                .statusCode(302)
+                .header("Location", containsString("https://accounts.google.com/o/oauth2/v2/auth"));
     }
 }
