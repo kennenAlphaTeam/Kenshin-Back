7c559fa7882392c84786939f1de049447d1c19dd
InChae Na <nicnic4102@gmail.com>
2022-03-04 13:46:49 +0900

    Dev (#33)
    * port가 8080이 아닐 때도 oauth test을 성공하도록 수정 - #27
    
    * x-rpc header를 properties에서 읽어오도록 수정

--- src/main/java/com/kennenalphateam/genshin/mihoyo/api/MihoyoApiInterceptor.java
+++ src/main/java/com/kennenalphateam/genshin/mihoyo/api/MihoyoApiInterceptor.java
@@ -1,11 +1,13 @@
 package com.kennenalphateam.genshin.mihoyo.api;
 
 import com.kennenalphateam.genshin.mihoyo.MihoyoUtils;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,14 +16,23 @@ import java.io.IOException;
 @Component
 public class MihoyoApiInterceptor implements Interceptor {
 
+    @Value("${mihoyo.rpc-client_type}")
+    private String clientType;
+
+    @Value("${mihoyo.rpc-app_version}")
+    private String appVersion;
+
+    @Value("${mihoyo.rpc-language}")
+    private String language;
+
     @Override
     public Response intercept(Chain chain) throws IOException {
         Headers headers = chain.request().headers();
         Request request = chain.request().newBuilder()
                 .headers(headers)
-                .addHeader("x-rpc-language", "ko-kr")
-                .addHeader("x-rpc-client_type", "4")
-                .addHeader("x-rpc-app_version", "1.5.0")
+                .addHeader("x-rpc-language", language)
+                .addHeader("x-rpc-client_type", clientType)
+                .addHeader("x-rpc-app_version", appVersion)
                 .addHeader("DS", MihoyoUtils.generateDSToken())
                 .build();
         Response response = chain.proceed(request);
