990ba626004f8c0033d486364e9b665e0d23aaf0
skdlsco <nicnic4102@gmail.com>
2022-03-24 15:21:11 +0900

    Swagger 에서 @LoginUser 값이 노출되지 않게 수정  - #61

--- src/main/java/com/kennenalphateam/genshin/user/LoginUser.java
+++ src/main/java/com/kennenalphateam/genshin/user/LoginUser.java
@@ -1,5 +1,7 @@
 package com.kennenalphateam.genshin.user;
 
+import io.swagger.v3.oas.annotations.Hidden;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,5 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@Hidden
 public @interface LoginUser {
 }
