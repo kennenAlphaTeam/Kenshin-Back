34884da328620cd9b81382d6be1baee060fb2892
skdlsco <nicnic4102@gmail.com>
2022-03-24 15:23:22 +0900

    MihoyoController content-type 설정 - #67
    text에서 json으로 변경

--- src/main/java/com/kennenalphateam/genshin/mihoyo/MihoyoController.java
+++ src/main/java/com/kennenalphateam/genshin/mihoyo/MihoyoController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RequestMapping("mihoyo")
+@RequestMapping(value = "mihoyo", produces = "application/json")
 @RestController
 @RequiredArgsConstructor
 public class MihoyoController {
